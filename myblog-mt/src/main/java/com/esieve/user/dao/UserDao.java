6efbc8b840409257edf595f4defc7c3be91e5a4b
张秦遥 <772392166@qq.com>
2018-03-05 13:43:31 +0800

    replace xml to annotation for dao of user service

--- myblog-mt/src/main/java/com/esieve/user/dao/UserDao.java
+++ myblog-mt/src/main/java/com/esieve/user/dao/UserDao.java
@@ -1,6 +1,8 @@
 package com.esieve.user.dao;
 
 import com.esieve.user.bean.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -9,16 +11,22 @@ import java.util.List;
  */
 public interface UserDao {
 
+    @Select("SELECT * FROM user")
     List<User> getUsers();
 
+    @Select("SELECT user_id,username,image FROM user WHERE user_id=#{userId}")
     User getUserByUserId(int userId);
 
+    @Select("SELECT user_id,username,image FROM user WHERE username=#{username} AND password=#{password}")
     User getUserByUser(User user);
 
+    @Insert("INSERT INTO user VALUES (NULL,#{username},#{password},#{image})")
     int insertUser(User user);
 
+    @Insert("UPDATE user SET username=#{username}, password=#{password}, image=#{image} WHERE user_id=#{userId}")
     int updateUser(User user);
 
+    @Insert("DELETE FROM user WHERE user_id=#{userId}")
     int deleteUser(int userId);
 
 }
