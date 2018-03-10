3fbb350bc0e591a6deea08ae06e1b844ee139bbb
张秦遥 <772392166@qq.com>
2018-03-10 22:39:39 +0800

    add fe

---
+++ myblog-fe/src/main/java/com/esieve/interceptor/LoginInterceptor.java
@@ -0,0 +1,33 @@
+package com.esieve.interceptor;
+
+import com.esieve.user.bean.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by 77239 on 2017/4/3/0003.
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        HttpSession httpSession = httpServletRequest.getSession();
+        User user = (User) httpSession.getAttribute("curUser");
+        if (user != null) {
+            return true;
+        } else {
+            httpServletResponse.sendRedirect("/blog");
+            return false;
+        }
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
